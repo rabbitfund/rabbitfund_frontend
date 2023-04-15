2a892e47827ad010315fd9816ee5324ba1e3eef2
tony140407 <a32715368@gmail.com>
2023-04-16 00:03:21 +0800

    做到一半

---
+++ pages/admin/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>歡迎來到後台</h1>
+  <p>{{ data?.[0] }}</p>
+</template>
+<script setup>
+const { data } = await useAsyncData('test', () => $fetch('https://api.nuxtjs.dev/mountains'));
+</script>
