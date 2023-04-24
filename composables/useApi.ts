fd116aee584d62bc0d0b7086ce8dae0240c5d4fd
tony140407 <a32715368@gmail.com>
2023-04-24 21:39:07 +0800

    fix: 修正 API error 問題

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,42 +1,15 @@
 export default function useApi() {
   // 登入相關
-  const signin = (data) =>
-    useFetch('/signin', { method: 'POST', body: JSON.stringify(data) })
-      .then((res) => {
-        alert('登入成功');
-        return res;
-      })
-      .catch((err) => {
-        alert('登入失敗');
-        throw err;
-      });
-  const signup = (data) =>
-    useFetch('/signup', { method: 'POST', body: JSON.stringify(data) })
-      .then((res) => {
-        alert('註冊成功');
-        return res;
-      })
-      .catch((err) => {
-        alert('註冊失敗');
-        throw err;
-      });
+  const signin = (data) => useFetch('/signin', { method: 'POST', body: JSON.stringify(data) });
+  const signup = (data) => useFetch('/signup', { method: 'POST', body: JSON.stringify(data) });
 
   // 會員中心
   const getUserDetail = () =>
     useFetch('/me/user').catch(() => {
       navigateTo('/signin');
-      alert('token 過期，請重新登入');
     });
   const putUserDetail = (data) =>
-    useFetch('/me/user', { method: 'PUT', body: JSON.stringify(data) })
-      .then((res) => {
-        alert('修改成功');
-        return res;
-      })
-      .catch((err) => {
-        alert('修改失敗');
-        throw err;
-      });
+    useFetch('/me/user', { method: 'PUT', body: JSON.stringify(data) });
   return {
     signin,
     signup,
--- plugins/fetch-interceptors.client.ts
+++ plugins/fetch-interceptors.client.ts
@@ -4,6 +4,7 @@ export default defineNuxtPlugin((_nuxtApp) => {
   const runtimeConfig = useRuntimeConfig();
   const { API_BASE } = runtimeConfig.public;
   const authStore = useAuthStore();
+
   globalThis.$fetch = $fetch.create({
     baseURL: API_BASE,
     onRequest({ options }) {
@@ -12,7 +13,19 @@ export default defineNuxtPlugin((_nuxtApp) => {
       }
     },
     onRequestError({ error }) {
-      console.error(error);
+      alert(error);
+    },
+    onResponse({ response }) {
+      if (!response.ok) {
+        alert(response._data.msg);
+        return;
+      }
+      alert(response._data.msg);
+      return response._data;
+    },
+    onResponseError(context) {
+      console.dir(context);
+      // alert(context.error);
     }
   });
 });
