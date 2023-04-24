2a9a884976cb72c2009d7b2f5efb84cbdf9237a6
tony140407 <a32715368@gmail.com>
2023-04-24 21:12:49 +0800

    fix: add cousor-pointer

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -33,7 +33,7 @@ const authStore = useAuthStore();
             <NuxtLink
               v-if="authStore.token"
               @click="authStore.handleSignOut()"
-              class="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+              class="block cursor-pointer rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
             >
               登出
             </NuxtLink>
