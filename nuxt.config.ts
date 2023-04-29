a12efc9920fa0cc5dbecf703d067f05346a3a4fd
Clara Cai <clara.cai@tpisoftware.com>
2023-04-29 18:36:45 +0800

    feat: tailwind init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/tailwindcss'],
+  css: [
+    '~/assets/tailwind.css',
+  ],
   runtimeConfig: {
     public: {
       GOOGLE_CLIENT_ID: '',
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./components/**/*.vue', './layouts/**/*.vue', './pages/**/*.vue'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
