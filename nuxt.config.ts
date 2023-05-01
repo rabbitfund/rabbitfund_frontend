aa2d75a69de32b1b56c68f250819a6dfc940db80
Clara Cai <clara.cai@tpisoftware.com>
2023-05-01 21:08:14 +0800

    chore: add tailwind.config, tailwind.css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config',
+  },
   runtimeConfig: {
     public: {
       GOOGLE_CLIENT_ID: '',
