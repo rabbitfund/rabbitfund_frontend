f472e57de18d56975e96d52bda0413fb61f7ef81
tony140407 <a32715368@gmail.com>
2023-05-20 20:55:39 +0800

    feat: 全域 loading 最小時間設為1.3秒

--- stores/globalState.ts
+++ stores/globalState.ts
@@ -1,14 +1,30 @@
 import { defineStore } from 'pinia';
 
 export const useGlobalStateStore = defineStore('globalState', {
-  // stores: [useUserStore],
   state: () => ({
-    isShowFullscreenLoading: false
+    isShowFullscreenLoading: false,
+    loadingStartTime: 0
   }),
   actions: {
-    toggleFullscreenLoading(state = undefined) {
+    async toggleFullscreenLoading(state = undefined) {
+      const minimumWaitTime = 1300; // 最小等待时间，单位：毫秒
+
+      if (state === false || !this.isShowFullscreenLoading === false) {
+        const currentTime = new Date().getTime();
+        const loadingTimeDiff = currentTime - this.loadingStartTime;
+
+        if (loadingTimeDiff < minimumWaitTime) {
+          const additionalWaitTime = minimumWaitTime - loadingTimeDiff;
+          await new Promise((resolve) => setTimeout(resolve, additionalWaitTime));
+        }
+      }
+
       this.isShowFullscreenLoading =
         typeof state === 'boolean' ? state : !this.isShowFullscreenLoading;
+      
+      if (this.isShowFullscreenLoading) {
+        this.loadingStartTime = new Date().getTime();
+      }
     }
   }
 });
