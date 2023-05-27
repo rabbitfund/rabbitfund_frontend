eee80c44e2795d35c87627cd8f38b03351aaffe7
tony140407 <a32715368@gmail.com>
2023-05-27 20:07:35 +0800

    feat: navbar新增搜尋

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,36 +1,46 @@
 <script setup>
 const page = ref(1);
-const tag = ref('long');
-const projects = ref([]);
+const tag = ref('');
+const k = ref('');
 
-const handleGetProjects = async () => {
-  // 監聽變數重新發送請求
-  const { data } = await useAsyncData(
-    'projectList',
-    () =>
-      $fetch(`/projects`, {
-        params: {
-          page: page.value,
-          tag: tag.value
-        }
-      }),
-    {
-      watch: [page, tag]
-    }
-  );
+const route = useRoute();
 
-  if (data.value?.ok) {
-    projects.value = data.value.data;
-    console.log(projects);
-  }
-};
-onMounted(async () => {
-  await nextTick();
-  handleGetProjects();
+watchEffect(() => {
+  tag.value = route.query.tag ? route.query.tag : '';
+  k.value = route.query.k ? route.query.k : '';
+
+  // 當 tag 或 k 變化時，將 page 設置為 1
+  page.value = 1;
 });
+
+const { data: projects } = await useAsyncData(
+  'projectList' + page.value + '_' + tag.value,
+  () =>
+    $fetch(`/projects`, {
+      params: {
+        page: page.value,
+        tag: tag.value,
+        k: k.value
+      }
+    }),
+  {
+    watch: [page, tag, k],
+    transform: (_projects) => _projects.data,
+    server: false
+  }
+);
 </script>
 <template>
-  <h2 class="mb-4">專案列表</h2>
+  <div class="container mx-auto my-6 flex justify-between lg:my-12">
+    <h2 class="">專案列表</h2>
+    <select v-model="tag" class="w-[120px]" value="">
+      <option value="">全部</option>
+      <option value="recent">近期</option>
+      <option value="hot">熱門</option>
+      <option value="long">長期募資</option>
+    </select>
+  </div>
+
   <section
     v-if="projects?.length && projects.length > 0"
     class="container grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3"
