4662f70b7f3722ce39ff42908f90350d11164232
Clara Cai <clara.cai@tpisoftware.com>
2023-06-08 00:49:13 +0800

    feat: add modal component

--- components/Card/MemberProject.vue
+++ components/Card/MemberProject.vue
@@ -16,7 +16,57 @@ const { formatTimeLeft } = useSetProjectStatus(props.project);
 const formattedAmount = computed(() => {
   return props.project?.project_progress?.toLocaleString();
 });
+
+const sponsorDetail = {};
+sponsorDetail.value = {
+  _id: '647236aadcb8be554f46b385',
+  user: {
+    _id: '647c838a38e4b9d2a9bea8f7',
+    user_phone: '0900-000-000',
+    user_name: '歐媚佩',
+    user_email: 'hans_rodriguez34@yahoo.com'
+  },
+  ownerInfo: '6453d211c6ab8c4bfc26717d',
+  project: {
+    _id: '64637814b8f583fff70452bd',
+    project_title: '愛奇兒家庭社區共融中心集資計畫',
+    project_cover: 'cover URL'
+  },
+  option: {
+    _id: '6471da154fe1111a089b5324',
+    option_name: '貼心套套',
+    option_cover: '/img/option.img'
+  },
+  order_option_quantity: 1,
+  order_extra: 0,
+  order_total: 2290,
+  order_note: '這是備註',
+  order_create_date: '2023-05-27T16:57:37.546Z',
+  order_status: 0,
+  order_shipping_status: 0,
+  order_info: {
+    _id: '647236aadcb8be554f46b383',
+    payment_price: 2290,
+    payment_status: 2,
+    invoice_type: '電子載具',
+    invoice_carrier: '電子發票 - 條碼 (/RES8356)',
+    invoice_date: '2023-06-04T14:02:26.851+00:00',
+    invoice_number: 'MNW-43430602',
+    newebpay_timeStamp: '1685206699',
+    newebpay_escrowBank: '809',
+    newebpay_payBankCode: '809',
+    newebpay_payerAccount5Code: '12345',
+    payment_method: 'WEBATM'
+  }
+};
+
+const modalSponsorDetail = ref(null);
+
+const openModalSponsorDetail = () => {
+  modalSponsorDetail.value.openModal();
+};
 </script>
+
 <template>
   <section
     class="flex flex-col justify-between gap-12 rounded-lg border p-5 lg:flex-row lg:px-8 lg:py-6"
@@ -41,7 +91,7 @@ const formattedAmount = computed(() => {
           ></Badge
         ></span>
 
-        <h4 class="mt-2 font-bold text-grey-600">{{ props.project?.project_title }}</h4>
+        <h4 class="mt-2">{{ props.project?.project_title }}</h4>
         <div class="mt-6 flex justify-between gap-6 bg-light-emphasis px-5 py-3">
           <div>
             <p>贊助人次</p>
@@ -75,4 +125,6 @@ const formattedAmount = computed(() => {
       </div>
     </div>
   </section>
+  <a href="#" class="font-bold text-primary underline" @click="openModalSponsorDetail()">贊助者詳細資料</a>
+  <ModalSponsorDetail ref="modalSponsorDetail" :detail="sponsorDetail" />
 </template>
