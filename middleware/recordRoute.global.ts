fe2f05e9f3137fb543a8df0101ca62e26a80749a
tony140407 <a32715368@gmail.com>
2023-06-10 19:42:04 +0800

    fix: 優化導向問題及登出後錯誤問題

--- middleware/recordRoute.global.ts
+++ middleware/recordRoute.global.ts
@@ -1,8 +1,15 @@
 import { useGlobalStateStore } from '@/stores/globalState';
 
+const regex = /^\/users\//;
 export default defineNuxtRouteMiddleware((to, from) => {
   const globalState = useGlobalStateStore();
   if (to.path === '/users/signin') {
-    globalState.recordLastRoute(from.path);
+    let prevPath = from.path;
+    // 檢查上一個路由是否為 /users/ 相關
+    if (regex.test(from.path)) {
+      prevPath = '/';
+    }
+
+    globalState.recordLastRoute(prevPath);
   }
 });
