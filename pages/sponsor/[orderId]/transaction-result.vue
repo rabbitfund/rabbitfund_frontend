c7aedcaeb2dddf5116ec2ea32abd60529da62bab
showanne <showanne.e@gmail.com>
2023-06-11 12:50:46 +0800

    style: 調整交易回傳頁面

--- pages/sponsor/[orderId]/transaction-result.vue
+++ pages/sponsor/[orderId]/transaction-result.vue
@@ -29,7 +29,7 @@ order.value = {
   </section>
   <section class="bg-light-emphasis lg:bg-transparent">
     <div class="container gap-6 lg:flex">
-      <div class="mb-12 bg-light-emphasis pt-12 lg:mb-20 lg:w-2/3 lg:p-12 lg:pb-20">
+      <div class="mb-12 bg-light-emphasis pt-12 lg:mb-20 lg:w-1/2 lg:p-12 lg:pb-20">
         <div class="mb-6 flex items-center gap-2">
           <span><img src="~/assets/images/icons/bars.svg" alt="信用卡" class="w-8" /></span>
           <h2 class="text-h5">訂單資料</h2>
@@ -44,10 +44,17 @@ order.value = {
         <p class="mb-3 lg:text-lg">付款方式：{{ order.PaymentType }}</p>
         <p class="mb-3 lg:text-lg">付款時間：{{ order.PayTime }}</p>
       </div>
-      <div class="mb-16 border-grey-200 lg:w-1/3 lg:pt-12">
-        <div class="mb-6 rounded-lg bg-success-emphasis px-8 py-10 ring-1 ring-grey-200">
+      <div class="mb-16 border-grey-200 lg:w-1/2 lg:pt-12">
+        <CardBlockCarrot
+          :title="'感謝您的贊助'"
+          :text="'.'"
+          :btn="'查看贊助紀錄'"
+          :link="'/member/orders'"
+        />
+
+        <!-- <div class="mb-6 rounded-lg bg-success-emphasis px-8 py-10 ring-1 ring-grey-200">
           <img src="~/assets/images/rabbit.png" alt="rabbit" />
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
