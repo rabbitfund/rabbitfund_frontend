b628921ae14c1abc441cd840e1312be557cef8e3
Bearkern <haolin1224@gmail.com>
2023-06-13 19:21:13 +0800

    Revert "Feature/add form validation"

--- pages/users/signup.vue
+++ pages/users/signup.vue
@@ -3,19 +3,21 @@ import { decodeCredential } from 'vue3-google-login';
 import { useAuthStore } from '@/stores/auth';
 
 const { handleSignUp } = useAuthStore();
+const { errors, useFieldModel } = useVeeValidate();
+const [email, password, name] = useFieldModel(['email', 'password', 'name']);
 
-const formSchema = {
-  email: 'required|email',
-  密碼: 'required|min:8',
-  使用者名稱: 'required'
-};
+const inputState = reactive({
+  email: false,
+  password: false,
+  name: false
+});
 
-function handleSubmit(values) {
+function generalSignUp() {
   handleSignUp({
     method: 0,
-    email: values.email,
-    pass: values.密碼,
-    name: values.使用者名稱
+    email: email.value,
+    pass: password.value,
+    name: name.value
   });
 }
 
@@ -38,52 +40,70 @@ definePageMeta({
 </script>
 
 <template>
-  <section class="container max-w-lg">
-    <div class="my-8 rounded bg-light-emphasis drop-shadow md:my-16">
-      <div class="p-5 sm:px-20 lg:py-10">
-        <Form class="mb-8" :validation-schema="formSchema" v-slot="{ meta }" @submit="handleSubmit">
-          <h2 class="mb-6 flex items-center gap-2">
-            <span class="w-8"><img src="~/assets/images/icons/user-fill.svg" alt="user" /></span>
-            <span>註冊</span>
-          </h2>
-          <TextInput
-            label="電子信箱"
+  <section
+    class="mx-auto w-full max-w-[500px] overflow-hidden rounded-2xl bg-gray-100 drop-shadow-md"
+  >
+    <div class="p-12 sm:px-20">
+      <div class="">
+        <h2 class="mb-5 border-b-2 border-lime-500 text-xl font-extrabold text-lime-500">註冊</h2>
+        <div class="mb-3">
+          <input
+            id="userEmail"
+            v-model="email"
             type="email"
-            id="email"
-            name="email"
-            placeholder="rabbit@example.com"
+            class="form-control m-auto block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:shadow-lg focus:outline-none"
+            placeholder="使用者帳號"
+            @focus="() => (inputState.email = true)"
           />
-          <TextInput
-            label="密碼"
-            type="password"
-            id="password"
-            name="密碼"
-            placeholder="請輸入密碼"
-          />
-          <TextInput
-            label="使用者名稱"
-            type="text"
-            id="name"
-            name="使用者名稱"
-            placeholder="請輸入使用者名稱"
-          />
-          <button class="btn btn-primary mt-2 w-full" :disabled="!meta.valid" type="submit">
-            註冊
-          </button>
-        </Form>
-
-        <div class="mb-6 flex font-bold text-primary">
-          <NuxtLink to="/users/signin" class="hover:text-primary-dark hover:underline"
-            >返回登入</NuxtLink
-          >
+          <label v-if="inputState.email" for="userEmail" class="text-sm text-red-500">{{
+            errors.email
+          }}</label>
         </div>
+        <div class="mb-3">
+          <input
+            id="userPassword"
+            v-model="password"
+            type="password"
+            class="form-control m-auto block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:shadow-lg focus:outline-none"
+            placeholder="密碼"
+            @focus="() => (inputState.password = true)"
+          />
+          <label v-if="inputState.password" for="userPassword" class="text-sm text-red-500">{{
+            errors.password
+          }}</label>
+        </div>
+        <div class="mb-3">
+          <input
+            id="userName"
+            v-model="name"
+            type="text"
+            class="form-control m-auto block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:shadow-lg focus:outline-none"
+            placeholder="使用者名稱"
+            @focus="() => (inputState.name = true)"
+          />
+          <label v-if="inputState.name" for="userName" class="text-sm text-red-500">{{
+            errors.name
+          }}</label>
+        </div>
+        <button
+          class="mb-2 w-full rounded-md bg-lime-500 py-1 font-bold text-white transition-all duration-200 ease-in-out hover:bg-white hover:text-lime-500 disabled:cursor-not-allowed disabled:bg-red-300 disabled:text-white"
+          :disabled="errors.email || errors.password || errors.name"
+          @click="generalSignUp"
+        >
+          註冊
+        </button>
 
-        <div>
-          <p class="mb-6 border-b border-grey-200 text-h5 text-info">或使用以下方式註冊</p>
+        <NuxtLink to="/users/signin" class="cursor-pointer text-lime-500 hover:text-lime-700"
+          >返回登入</NuxtLink
+        >
+
+        <div class="mt-7">
+          <p class="text-l mb-5 border-b-2 border-lime-500 font-extrabold text-lime-500">
+            或使用以下方式註冊
+          </p>
+
           <ClientOnly>
-            <div class="text-center">
-              <GoogleLogin :callback="googleSignUp" />
-            </div>
+            <GoogleLogin :callback="googleSignUp" />
           </ClientOnly>
         </div>
       </div>
