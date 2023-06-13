558cf17b4a1c8e499ba6550b47b6d4bf9f8dd495
tony140407 <a32715368@gmail.com>
2023-06-13 20:19:31 +0800

    feat: 新增錯誤訊息log

--- error.vue
+++ error.vue
@@ -3,3 +3,13 @@
     <CardBlockRabbit :title="'發生了一點錯誤'" :btn="'回到首頁'" :link="'/'" />
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+});
+
+onMounted(() => {
+  console.error('[捕獲錯誤]', props.error.message);
+});
+</script>
