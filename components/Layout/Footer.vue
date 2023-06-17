85bbc7670a2267b4cac703ce49fb58f2463a5b66
Clara Cai <clara.cai@tpisoftware.com>
2023-06-18 02:25:12 +0800

    fix: fix Footer styles. Add link hover style

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,31 +1,44 @@
 <template>
-  <section class="bg-primary text-white">
-    <div class="container flex flex-col justify-between py-9 md:flex-row">
-      <div class="flex flex-row items-center justify-between md:flex-col md:justify-start">
-        <img class="mb-5 inline-block h-[58px]" src="@/assets/images/logo-white.svg" alt="logo" />
-        <div class="flex gap-5">
-          <NuxtLink><img src="@/assets/images/icons/youtube.svg" alt="youtube" /></NuxtLink>
-          <NuxtLink><img src="@/assets/images/icons/instagram.svg" alt="instagram" /></NuxtLink>
-          <NuxtLink><img src="@/assets/images/icons/facebook.svg" alt="facebook" /></NuxtLink>
+  <footer class="mt-auto bg-primary text-white lg:text-lg">
+    <nav class="container flex flex-col justify-between py-10 md:flex-row md:items-center">
+      <div
+        class="mb-6 flex flex-row items-center justify-between md:mb-0 md:flex-col md:gap-2 lg:gap-4"
+      >
+        <img
+          class="inline-block h-[58px] self-start"
+          src="@/assets/images/logo-white.svg"
+          alt="logo"
+        />
+        <div class="flex gap-4">
+          <NuxtLink class="link"
+            ><img src="@/assets/images/icons/youtube.svg" alt="youtube"
+          /></NuxtLink>
+          <NuxtLink class="link"
+            ><img src="@/assets/images/icons/instagram.svg" alt="instagram"
+          /></NuxtLink>
+          <NuxtLink class="link"
+            ><img src="@/assets/images/icons/facebook.svg" alt="facebook"
+          /></NuxtLink>
         </div>
       </div>
-      <div class="flex items-center">
-        <div class="mx-auto py-3">
-          <div class="flex gap-x-6 md:gap-x-12">
-            <NuxtLink to="/projects">探索</NuxtLink>
-            <NuxtLink to="/about">關於我們</NuxtLink>
-            <NuxtLink to="/about">贊助保障方案</NuxtLink>
-            <NuxtLink to="/about">使用者條款</NuxtLink>
-          </div>
-          <div class="flex gap-x-6 md:gap-x-12">
-            <NuxtLink to="/about">提案</NuxtLink>
-            <NuxtLink to="/faq">常見問題</NuxtLink>
-            <NuxtLink to="/about">提案服務說明</NuxtLink>
-            <NuxtLink to="/privacy">隱私權政策</NuxtLink>
-          </div>
+      <div class="flex flex-col items-center gap-4">
+        <div class="flex gap-x-6 md:gap-x-12">
+          <NuxtLink to="/projects" class="link">探索</NuxtLink>
+          <NuxtLink to="/about" class="link">關於我們</NuxtLink>
+          <NuxtLink to="/about" class="link">贊助保障方案</NuxtLink>
+          <NuxtLink to="/about" class="link">使用者條款</NuxtLink>
+        </div>
+        <div class="flex gap-x-6 md:gap-x-12">
+          <NuxtLink to="/about" class="link">提案</NuxtLink>
+          <NuxtLink to="/faq" class="link">常見問題</NuxtLink>
+          <NuxtLink to="/about" class="link">提案服務說明</NuxtLink>
+          <NuxtLink to="/privacy" class="link">隱私權政策</NuxtLink>
         </div>
       </div>
-    </div>
-    <div class="bg-primary-dark text-center">Copyright ©2023 北二有限公司</div>
-  </section>
+    </nav>
+
+    <p class="bg-primary-dark py-6 text-center text-sm lg:text-lg">
+      Copyright ©2023 北二有限公司
+    </p>
+  </footer>
 </template>
