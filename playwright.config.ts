67d344159dc6bc489513889efab5ebe427f2eefc
johnlin <john830110@gmail.com>
2023-06-20 17:07:18 +0800

    test: 測試 playwright 設定

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:8085'
-    // reuseExistingServer: !process.env.CI
+    url: 'http://127.0.0.1:8085',
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI
   }
 });
