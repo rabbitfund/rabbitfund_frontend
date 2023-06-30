cff594fe080918f1698a7a0da74aa1aff6d9aa9f
Clara Cai <clara.cai@tpisoftware.com>
2023-07-01 00:02:30 +0800

    refactor: 更改會員中心的 HTML 結構

--- pages/member.vue
+++ pages/member.vue
@@ -6,61 +6,75 @@ definePageMeta({
 </script>
 
 <template>
-  <section class="container grid grid-cols-12 gap-8 py-6 md:py-16">
+  <div class="grid grid-cols-12 lg:container lg:gap-6 lg:py-16">
     <aside
-      class="col-span-12 flex flex-row gap-1 overflow-auto overflow-y-hidden whitespace-nowrap px-3 py-1 text-grey-400 lg:col-span-3 lg:flex-col lg:gap-4 lg:rounded-xl lg:bg-light-emphasis lg:px-4 lg:py-6"
+      class="col-span-12 mb-4 border-y border-grey-200 lg:col-span-3 lg:rounded-xl lg:border-0 lg:bg-light-emphasis lg:mb-0"
     >
-      <NuxtLink
-        to="/member"
-        class="aside-default"
-        :class="{ 'active-aside': route.path === '/member' }"
-        >會員中心</NuxtLink
+      <ul
+        class="flex flex-row overflow-auto whitespace-nowrap px-3 py-6 text-grey-400 lg:flex-col lg:gap-4 lg:px-4"
       >
-      <NuxtLink
-        to="/member/proposals"
-        class="aside-default"
-        :class="{ 'active-aside': route.path.includes('/member/proposals') }"
-        >提案紀錄</NuxtLink
-      >
-      <NuxtLink
-        to="/member/orders"
-        class="aside-default"
-        :class="{ 'active-aside': route.path.includes('/member/orders') }"
-        >贊助紀錄</NuxtLink
-      >
-      <NuxtLink
-        to="/member/notifications"
-        class="aside-default"
-        :class="{ 'active-aside': route.path.includes('/member/notifications') }"
-        >通知中心</NuxtLink
-      >
-      <NuxtLink
-        to="/member/manage"
-        class="aside-default"
-        :class="{ 'active-aside': route.path.includes('/member/manage') }"
-        >管理個人資料</NuxtLink
-      >
-      <!-- 
-      <NuxtLink
-        to="/member/change-password"
-        class="aside-default"
-        :class="{ 'active-aside': route.path.includes('/member/change-password') }"
-        >密碼變更</NuxtLink
-      >
-       -->
+        <li>
+          <NuxtLink
+            to="/member"
+            class="aside-default"
+            :class="{ 'active-aside': route.path === '/member' }"
+            >會員中心
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/member/proposals"
+            class="aside-default"
+            :class="{ 'active-aside': route.path.includes('/member/proposals') }"
+            >提案紀錄
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/member/orders"
+            class="aside-default"
+            :class="{ 'active-aside': route.path.includes('/member/orders') }"
+            >贊助紀錄
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/member/notifications"
+            class="aside-default"
+            :class="{ 'active-aside': route.path.includes('/member/notifications') }"
+            >通知中心
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/member/manage"
+            class="aside-default"
+            :class="{ 'active-aside': route.path.includes('/member/manage') }"
+            >管理個人資料
+          </NuxtLink>
+        </li>
+        <!-- <li>
+          <NuxtLink
+            to="/member/change-password"
+            class="aside-default"
+            :class="{ 'active-aside': route.path.includes('/member/change-password') }"
+            >密碼變更
+          </NuxtLink>
+        </li> -->
+      </ul>
     </aside>
     <div class="col-span-12 lg:col-span-9">
       <NuxtPage />
     </div>
-  </section>
+  </div>
 </template>
 
 <style scoped>
 .aside-default {
-  @apply rounded-lg px-4 py-2 text-lg hover:bg-light hover:font-bold hover:text-grey-600;
+  @apply inline-block rounded-lg px-4 py-2 text-lg hover:bg-light hover:font-bold hover:text-grey-600 lg:w-full;
 }
 .active-aside {
-  @apply bg-light text-lg font-bold text-grey-600;
+  @apply bg-light font-bold text-grey-600;
 }
 
 /* custom scrollbar */
