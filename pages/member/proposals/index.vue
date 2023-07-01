35cefa0323aa5432856ecddc6c784ee0b1fca05c
Clara Cai <clara.cai@tpisoftware.com>
2023-07-01 14:40:08 +0800

    feat: 完成提案紀錄 RWD

--- pages/member/proposals/index.vue
+++ pages/member/proposals/index.vue
@@ -16,17 +16,25 @@ getOwnerProject()
 </script>
 
 <template>
-  <!-- <h1>提案紀錄</h1> -->
-  <div v-if="project.length > 0" class="flex flex-col gap-4">
-    <CardMemberProject
-      v-for="i in project"
-      :key="i._id + i"
-      :can-modify="true"
-      :project="i"
-      :navigate-to-path="`/member/proposals/${i._id}`"
-    />
+  <div class="container">
+    <h2 class="my-6 flex items-center gap-2 lg:mt-0 lg:mb-12">
+      <span class="w-8"><img src="~/assets/images/icons/detail.svg" alt="note" /></span>
+      <span>提案紀錄</span>
+    </h2>
   </div>
-  <div v-else>
+
+  <div v-if="project.length > 0" class="bg-light-emphasis lg:bg-transparent">
+    <ul class="container flex flex-col gap-6 py-12 md:flex-row md:flex-wrap lg:py-0 xl:flex-col">
+      <li v-for="i in project" :key="i._id + i" class="md:w-[calc(50%-12px)] xl:w-full">
+        <CardMemberProject
+          :project="i"
+          :can-modify="true"
+          :navigate-to-path="`/member/proposals/${i._id}`"
+        />
+      </li>
+    </ul>
+  </div>
+  <div v-else class="py-12 lg:py-0">
     <CardBlockRabbit :title="'目前沒有提案紀錄'" :btn="'來去提案'" :link="'/proposal'" />
   </div>
 </template>
