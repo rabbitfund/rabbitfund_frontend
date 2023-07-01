b28438920b8a9650296fc7f0a0300cda2a21c6f0
Clara Cai <clara.cai@tpisoftware.com>
2023-07-02 00:30:57 +0800

    feat: 完成贊助紀錄 RWD

--- pages/member/orders.vue
+++ pages/member/orders.vue
@@ -29,19 +29,33 @@ watch([page], () => {
 });
 </script>
 <template>
-  <div v-if="order.length !== 0" class="flex flex-col gap-4">
-    <CardMemberOrder v-for="i in order" :key="i._id + i" :order="i" />
-    <LayoutPagination
-      :total-page="totalPages"
-      :current-page="page"
-      :handle-page-change="
-        (i) => {
-          page = i;
-        }
-      "
-    />
+  <div class="container">
+    <h2 class="my-6 flex items-center gap-2 lg:mb-12 lg:mt-0">
+      <span class="w-8"><img src="~/assets/images/icons/detail.svg" alt="note" /></span>
+      <span>贊助紀錄</span>
+    </h2>
   </div>
-  <div v-else>
+
+  <div v-if="order.length !== 0" class="bg-light-emphasis lg:bg-transparent">
+    <ul class="container flex flex-col gap-6 py-12 md:flex-row md:flex-wrap lg:py-0 xl:flex-col mb-8">
+      <li v-for="i in order" :key="i._id + i">
+        <CardMemberOrder :order="i" />
+      </li>
+    </ul>
+
+    <div class="text-center">
+      <LayoutPagination
+        :total-page="totalPages"
+        :current-page="page"
+        :handle-page-change="
+          (i) => {
+            page = i;
+          }
+        "
+      />
+    </div>
+  </div>
+  <div v-else class="py-12 lg:py-0">
     <CardBlockCarrot :title="'目前沒有贊助紀錄'" :btn="'來去贊助'" :link="'/projects'" />
   </div>
 </template>
